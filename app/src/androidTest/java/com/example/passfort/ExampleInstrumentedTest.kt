b61c55861894d7eb94e029654d1929be7e1e1ec1
timur <timurs.kzn@gmail.com>
2025-04-06 13:49:30 +0300

    removed test;

--- app/src/androidTest/java/com/example/passfort/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/passfort/ExampleInstrumentedTest.kt
@@ -1,19 +1,8 @@
 package com.example.passfort
 
-import androidx.room.Room
-import androidx.test.platform.app.InstrumentationRegistry
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.passfort.dbentity.PasswordRecordEntity
-import com.example.passfort.model.PassFortDB
-import com.example.passfort.model.dao.PasswordDao
-import kotlinx.coroutines.runBlocking
-
-import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Before
-import java.time.LocalDate
-import java.time.LocalDateTime
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -22,20 +11,5 @@ import java.time.LocalDateTime
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    private lateinit var passwordDao: PasswordDao
-    private lateinit var db: PassFortDB
 
-    @Before
-    fun createDb() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        db = Room.inMemoryDatabaseBuilder(appContext, PassFortDB::class.java).build()
-        passwordDao = db.getPasswordDao()
-    }
-
-    @Test
-    fun useAppContext() {
-        runBlocking { passwordDao.upsertObj(
-            PasswordRecordEntity(0, "name1", "login1", "password1", LocalDate.now(), 60, 1, false, LocalDateTime.now())
-        ) }
-    }
 }
