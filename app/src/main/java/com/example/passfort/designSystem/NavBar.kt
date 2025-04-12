4b74a15d3cc468b36060ca9e6298d4492abfa53e
kontsevich-daniil <Vaiper322@yandex.ru>
2025-04-12 21:54:18 +0300

    fixs

--- app/src/main/java/com/example/passfort/designSystem/NavBar.kt
+++ app/src/main/java/com/example/passfort/designSystem/NavBar.kt
@@ -1,123 +1,132 @@
 package com.example.passfort.designSystem
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.example.passfort.R
+import com.example.passfort.designSystem.theme.PassFortTheme
 
+@Immutable
 data class NavigationBarItem(
-    val nameOpenActivity: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector
+    @StringRes val nameOpenActivity: Int,
+    @DrawableRes val Icon: Int,
 )
-
 
 @Preview
 @Composable
 fun NavigationBar() {
     val navItems = listOf(
         NavigationBarItem(
-            nameOpenActivity = "",
-            selectedIcon = ImageVector.vectorResource(R.drawable.navbar_home_selected),
-            unselectedIcon = ImageVector.vectorResource(R.drawable.navbar_home_unselected),
+            nameOpenActivity = 0,
+            Icon = R.drawable.navbar_home,
         ),
         NavigationBarItem(
-            nameOpenActivity = "",
-            selectedIcon = ImageVector.vectorResource(R.drawable.navbar_passwords_selected),
-            unselectedIcon = ImageVector.vectorResource(R.drawable.navbar_passwords_unselected)
+            nameOpenActivity = 0,
+            Icon = R.drawable.navbar_passwords,
         ),
         NavigationBarItem(
-            nameOpenActivity = "",
-            selectedIcon = ImageVector.vectorResource(R.drawable.navbar_key_selected),
-            unselectedIcon = ImageVector.vectorResource(R.drawable.navbar_key_unselected)
+            nameOpenActivity = 0,
+            Icon = R.drawable.navbar_key,
         ),
         NavigationBarItem(
-            nameOpenActivity = "",
-            selectedIcon = ImageVector.vectorResource(R.drawable.navbar_settings_selected),
-            unselectedIcon = ImageVector.vectorResource(R.drawable.navbar_settings_unselected)
+            nameOpenActivity = 0,
+            Icon = R.drawable.navbar_settings,
         )
     )
 
     val selectedItemByIndex by remember {
-        mutableStateOf(0)
+        mutableStateOf(1)
     }
 
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.White, RoundedCornerShape(40.dp))
-            .height(80.dp)
-            .padding(horizontal = 20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 20.dp)
+            .shadow(5.dp, shape= CircleShape)
     ) {
-        navItems.forEachIndexed { index, item ->
-            NavItem(item.selectedIcon, item.unselectedIcon, index == selectedItemByIndex, {})
-        if (index == 1){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White, RoundedCornerShape(40.dp))
+                .height(80.dp)
+                .padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            navItems.forEachIndexed { index, item ->
+                NavItem(ImageVector.vectorResource(item.Icon), index == selectedItemByIndex, {})
 
-            Box(
-                modifier = Modifier
-                    .offset { IntOffset(x = 0, y = -60) }
-                    .clickable {  }
-                    .size(80.dp)
-                    .align(Alignment.Bottom)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.primary, RoundedCornerShape(35.dp))
-                ) {
-                    Icon(
+                if (index == 1) {
+                    Box(
                         modifier = Modifier
-                            .fillMaxSize(),
-                        imageVector = ImageVector.vectorResource(R.drawable.icon_button_add),
-                        tint = MaterialTheme.colorScheme.surface,
-                        contentDescription = null,
-                    )
+                            .offset { IntOffset(x = 0, y = -60) }
+                            .clickable { }
+                            .size(80.dp)
+                            .align(Alignment.Bottom)
+                            .padding(5.dp)
+                            .shadow(10.dp, shape= CircleShape)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .fillMaxWidth()
+                                .background(
+                                    MaterialTheme.colorScheme.primary,
+                                    RoundedCornerShape(35.dp)
+                                )
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                imageVector = ImageVector.vectorResource(R.drawable.icon_button_add),
+                                tint = MaterialTheme.colorScheme.surface,
+                                contentDescription = null,
+                            )
+                        }
+                    }
                 }
             }
-        }
         }
     }
 }
 
 @Composable
-fun NavItem(iconSelected: ImageVector,
-            iconUnselected: ImageVector,
+fun NavItem(iconImage: ImageVector,
             isSelected: Boolean,
             onClick: () -> Unit) {
-    val iconImage = if (isSelected) iconSelected else iconUnselected
+
     val iconColor = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
 
     Box(
@@ -135,4 +144,10 @@ fun NavItem(iconSelected: ImageVector,
             contentDescription = null,
         )
     }
+}
+
+@PreviewLightDark
+@Composable
+fun PrevieNavBar(){
+    PassFortTheme { NavigationBar() }
 }
